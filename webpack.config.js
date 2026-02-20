fd76225f79f6b5e919437a74b567578ab679e8be
JGON110 <jgon110@bancodebogota.com.co>
2026-02-19 20:43:09 -0500

    test webpack

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.tsx',  // ← tsx
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist',
+        publicPath: 'https://xxxx.cloudfront.net/mfe2/',
+        clean: true,
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx|js|jsx)$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.svg$/,
+                type: 'asset/resource',
+            }
+        ]
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'mfe2',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './App': './src/App.tsx',
+            },
+            shared: {
+                react: { singleton: true, eager: true },
+                'react-dom': { singleton: true, eager: true },
+            },
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
+}
